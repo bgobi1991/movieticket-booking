63712a22a5c17bd9747b491fb65e6f04c0539505
gobi <bgobi1991@gmail.com>
2017-07-03 11:10:27 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -29,18 +29,35 @@ router.get('/bookings', function (req, res, next) {
 });
 
 router.post('/book', function (req, res) {
-
-    fs.writeFile("data/bookings/"+req.body.movie_id+".json", JSON.stringify(req.body) , function(err, data) {
-        if(err) {
+    ensureExists('./data/bookings', 0744, function(err) {
+        if (err) {
             res.end(JSON.stringify({"status": 0, "msg": "Failed to booking", "type": "error"}));
+        } else {
+            fs.writeFile("data/bookings/"+req.body.movie_id+".json", JSON.stringify(req.body) , function(err, data) {
+                if(err) {
+                    res.end(JSON.stringify({"status": 0, "msg": "Failed to booking", "type": "error"}));
+                }
+                res.end(JSON.stringify({"status": 1, "msg": "Booking successfully", "type": "success"}));
+            });
         }
-        res.end(JSON.stringify({"status": 1, "msg": "Booking successfully", "type": "success"}));
     });
 }); 
 
 function requireUncached(module){
     delete require.cache[require.resolve(module)]
     return require(module)
+}
+function ensureExists(path, mask, cb) {
+    if (typeof mask == 'function') { // allow the `mask` parameter to be optional
+        cb = mask;
+        mask = 0777;
+    }
+    fs.mkdir(path, mask, function(err) {
+        if (err) {
+            if (err.code == 'EEXIST') cb(null); // ignore the error if the folder already exists
+            else cb(err); // something else went wrong
+        } else cb(null); // successfully created folder
+    });
 }
 
 module.exports = router;
